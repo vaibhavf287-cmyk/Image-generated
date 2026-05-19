36f9d466cf623a6113dd1d482ec830afd581c9de
vaibhavf287-cmyk <vaibhavf287@gmail.com>
2026-05-19 16:01:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import io
 app = Flask(__name__)
 
 API_URL = "https://huggingface.co"
-headers = {"Authorization": f"Bearer {os.environ.get('HF_API_KEY')}"}
+headers = {"Authorization": f"Bearer {os.environ.get('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')}"}
 
 # सुंदर और सिंपल HTML UI डिजाइन
 HTML_UI = """
